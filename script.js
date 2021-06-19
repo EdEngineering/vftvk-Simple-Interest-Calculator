e9e63c8ffe4bdf68c5a7ea6cdca51d3a87864476
EdEngineering <eduar_vasquez04@hotmail.com>
2021-06-19 15:53:36 -0600

    Completed Project

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+const compute = document.getElementById("compute");
+
+compute.addEventListener('click',function() {
+    console.log('Computing...');
+    var amount = document.getElementById("amount");
+    var result = document.getElementById("result");
+    var rate = document.getElementById("rate");
+    var years = document.getElementById("years");
+    if (amount.value == "") {
+        alert('Please fill the amount data');
+        amount.focus();
+    }
+    else{
+        result.innerHTML = "If you deposit <mark>"+amount.value+"</mark>";
+        result.innerHTML +="<br>at an interest rate of <mark>"+rate.value+"% </mark>";
+        result.innerHTML += "<br>You will receive an amount of <mark>"+(parseInt((amount.value)*(rate.value/100)*(years.value)))+"</mark>";
+        result.innerHTML +="<br>in the year <mark>"+(2021+parseInt(years.value))+"</mark>";
+    }
+});
+
         
